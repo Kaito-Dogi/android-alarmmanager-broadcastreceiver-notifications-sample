fd357c2bc33f0f1bab1eb41036579a89feadc02a
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-18 01:07:24 +0900

    add: BroadcastReceiverでアラームの通知を取得

---
+++ app/src/main/java/app/doggy/alarmmanagersample/AlarmBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package app.doggy.alarmmanagersample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "onReceive()", Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
+++ app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
@@ -1,8 +1,12 @@
 package app.doggy.alarmmanagersample
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.doggy.alarmmanagersample.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.setButton.setOnClickListener {
+            // 通知を送る時刻を，ボタンを押した10秒後に設定．
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = System.currentTimeMillis()
+            calendar.add(Calendar.SECOND, 10)
+
+            //
+            val pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                Intent(this, AlarmBroadcastReceiver::class.java),
+                0
+            )
+
+            // アラームをセットする．
+            val alarmManager: AlarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        }
     }
 }
