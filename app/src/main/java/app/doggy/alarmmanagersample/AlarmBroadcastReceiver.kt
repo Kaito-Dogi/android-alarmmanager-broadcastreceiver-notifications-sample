c2049f9b4580b720d025c77acccd355bc3bdea98
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-18 07:58:19 +0900

    clean: コメントを追記

--- app/src/main/java/app/doggy/alarmmanagersample/AlarmBroadcastReceiver.kt
+++ app/src/main/java/app/doggy/alarmmanagersample/AlarmBroadcastReceiver.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
 
+    // アラームを検知．
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "onReceive()", Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
+++ app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import app.doggy.alarmmanagersample.databinding.ActivityMainBinding
 import java.util.Calendar
 
@@ -17,22 +18,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
         binding.setButton.setOnClickListener {
-            // 通知を送る時刻を，ボタンを押した10秒後に設定．
+            // Calendarクラスのインスタンスを取得．
             val calendar = Calendar.getInstance()
+            // 現在の時間をミリ秒で取得．
             calendar.timeInMillis = System.currentTimeMillis()
+            // 10秒後に設定．
             calendar.add(Calendar.SECOND, 10)
+            // calendar.set(Calendar.HOGE, amount)で時間，分などを指定できる．
 
-            //
+            // アラームがトリガーされたときに開始するペンディングインテント．
             val pendingIntent = PendingIntent.getBroadcast(
                 this,
                 0,
                 Intent(this, AlarmBroadcastReceiver::class.java),
-                0
+                PendingIntent.FLAG_UPDATE_CURRENT
             )
 
-            // アラームをセットする．
+            // AlarmManagerをインスタンス化する．
             val alarmManager: AlarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+            /** アラームをセットする．
+             *
+             * アラームの頻度は最小限に維持する．
+             * アラームのトリガー時間を必要以上に正確に設定しない．
+             * 正確なトリガー時刻に基づく反復アラームはうまく調整できないため，可能であれば時刻に基づいてアラームをトリガーすることは避ける．
+             *
+             * 1回だけ（反復なし）のアラーム：アラームタイプ，トリガー時刻，ペンディングインテントを渡す．
+             * set()：推奨．APIレベル19以降では低精度．
+             * setExact()：setよりも正確．
+             *
+             * 繰り返しアラーム：アラームタイプ，トリガー時刻，アラームの間隔，ペンディングインテントを渡す．
+             * setInexactRepeating()：推奨．Androidによって複数の不正確な反復アラームが同期され，それらが同時にトリガーされることにより，電池の消耗を抑えることができる．
+             * setRepeating()：厳密な時間要件がある場合に使用する．
+             *
+             * アラームタイプ
+             * ELAPSED_REALTIME：デバイスが起動してから指定された時間が経過した後にペンディングインテントを開始するが，デバイスのスリープは解除しない．経過時間にはデバイスがスリープしていた時間が含まれる．
+             * ELAPSED_REALTIME_WAKEUP：デバイスが起動してから指定された時間が経過した後にデバイスのスリープを解除してペンディングインテントを開始する．
+             * RTC：指定された時刻にペンディングインテントを開始するが，デバイスのスリープは解除しない．
+             * RTC_WAKEUP：指定された時刻にデバイスのスリープを解除してペンディングインテントを開始する．
+             */
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+
+            Toast.makeText(this, "10秒後にアラームをセット", Toast.LENGTH_SHORT).show()
         }
     }
 }
