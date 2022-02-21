4e1c5c74ec0ab16382e0461472a6b01c6a14f383
Kaito-Dogi <kendobu0405@gmail.com>
2022-02-22 01:00:41 +0900

    add: PendingIntent.FLAG_UPDATE_CURRENT以外のコメントを追記

--- app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
+++ app/src/main/java/app/doggy/alarmmanagersample/MainActivity.kt
@@ -16,21 +16,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var alarmManager: AlarmManager
 
-    private val requestCode: Int = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
         binding.setButton.setOnClickListener {
-            // Calendarクラスのインスタンスを取得．
+            // Calendarクラスのインスタンスを取得。
             val calendar = Calendar.getInstance()
-            // 現在の時間をミリ秒で取得．
+            // 現在の時間をミリ秒で取得。
             calendar.timeInMillis = System.currentTimeMillis()
-            // 5秒後に設定．
+            // 5秒後に設定。
             calendar.add(Calendar.SECOND, 5)
 
-            /** calendar.set(Calendar.CONSTANT, amount)で日付と時刻を指定できる．
+            /**
+             * calendar.set(Calendar.CONSTANT, amount)で日付と時刻を指定できる。
              *
              * 例：2021年12月19日午後3時20分 日本標準時
              * calendar.set(Calendar.YEAR, 2021)
@@ -42,55 +41,66 @@ class MainActivity : AppCompatActivity() {
              * calendar.timeZone = TimeZone.getTimeZone("Asia/Tokyo")
              */
 
-            // アラームがトリガーされたときに開始するペンディングインテント．
+            // アラームがトリガーされたときに開始するペンディングインテント。
             val pendingIntent = PendingIntent.getBroadcast(
                 this,
-                requestCode,
+                // requestCodeの値で、Receiver側で処理を切り分ける。
+                REQUEST_CODE,
+                // 明示的なブロードキャスト。
                 Intent(this, AlarmBroadcastReceiver::class.java).putExtra(
-                    REQUEST_CODE,
-                    requestCode
+                    REQUEST_CODE_KEY,
+                    REQUEST_CODE
                 ),
+                //
                 PendingIntent.FLAG_UPDATE_CURRENT
             )
 
-            // AlarmManagerをインスタンス化する．
+            // AlarmManagerをインスタンス化する。
             alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
-            /** アラームをセットする．
+            /**
+             * アラームをセットする。
              *
-             * アラームの頻度は最小限に維持する．
-             * アラームのトリガー時間を必要以上に正確に設定しない．
-             * 正確なトリガー時刻に基づく反復アラームはうまく調整できないため，可能であれば時刻に基づいてアラームをトリガーすることは避ける．
+             * アラームの頻度は最小限に維持する。
+             * アラームのトリガー時間を必要以上に正確に設定しない。
+             * 正確なトリガー時刻に基づく反復アラームはうまく調整できないため、可能であれば時刻に基づいてアラームをトリガーすることは避ける。
              *
-             * 1回だけ（反復なし）のアラーム：アラームタイプ，トリガー時刻，ペンディングインテントを渡す．
-             * set()：推奨．APIレベル19以降では低精度．
-             * setExact()：setよりも正確．
+             * 1回だけ（反復なし）のアラーム：アラームタイプ，トリガー時刻，ペンディングインテントを渡す。
+             * set()：推奨．APIレベル19以降では低精度。
+             * setExact()：setよりも正確。
              *
-             * 繰り返しアラーム：アラームタイプ，トリガー時刻，アラームの間隔，ペンディングインテントを渡す．
-             * setInexactRepeating()：推奨．Androidによって複数の不正確な反復アラームが同期され，それらが同時にトリガーされることにより，電池の消耗を抑えることができる．
-             * setRepeating()：厳密な時間要件がある場合に使用する．
+             * 繰り返しアラーム：アラームタイプ，トリガー時刻，アラームの間隔，ペンディングインテントを渡す。
+             * setInexactRepeating()：推奨。Androidによって複数の不正確な反復アラームが同期され、それらが同時にトリガーされることにより、電池の消耗を抑えることができる。
+             * setRepeating()：厳密な時間要件がある場合に使用する。
              *
              * アラームタイプ
-             * ELAPSED_REALTIME：デバイスが起動してから指定された時間が経過した後にペンディングインテントを開始するが，デバイスのスリープは解除しない．経過時間にはデバイスがスリープしていた時間が含まれる．
-             * ELAPSED_REALTIME_WAKEUP：デバイスが起動してから指定された時間が経過した後にデバイスのスリープを解除してペンディングインテントを開始する．
-             * RTC：指定された時刻にペンディングインテントを開始するが，デバイスのスリープは解除しない．
-             * RTC_WAKEUP：指定された時刻にデバイスのスリープを解除してペンディングインテントを開始する．
+             * ELAPSED_REALTIME：デバイスが起動してから指定された時間が経過した後にペンディングインテントを開始するが、デバイスのスリープは解除しない。経過時間にはデバイスがスリープしていた時間が含まれる。
+             * ELAPSED_REALTIME_WAKEUP：デバイスが起動してから指定された時間が経過した後にデバイスのスリープを解除してペンディングインテントを開始する。
+             * RTC：指定された時刻にペンディングインテントを開始するが、デバイスのスリープは解除しない。
+             * RTC_WAKEUP：指定された時刻にデバイスのスリープを解除してペンディングインテントを開始する。
              */
             alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
             Log.d(ALARM_LOG, "alarmManager.set()")
 
             Toast.makeText(this, "5秒後にアラームをセット", Toast.LENGTH_SHORT).show()
+            /**
+             * 他にも以下のメソッドがある。
+             *
+             * cancel：設定したアラームの取り消し。
+             * setTime：ミリ秒での時間設定。
+             * setTimeZone：タイムゾーンの設定。
+             */
         }
 
         binding.cancelButton.setOnClickListener {
             val pendingIntent = PendingIntent.getBroadcast(
                 this,
-                requestCode,
+                REQUEST_CODE,
                 Intent(this, AlarmBroadcastReceiver::class.java),
                 PendingIntent.FLAG_NO_CREATE
             )
 
-            // アラームをキャンセルする．
+            // アラームをキャンセルする。
             alarmManager.cancel(pendingIntent)
             Log.d(ALARM_LOG, "alarmManager.cancel()")
 
@@ -99,7 +109,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val REQUEST_CODE = "REQUEST_CODE"
+        const val REQUEST_CODE_KEY = "REQUEST_CODE"
+        const val REQUEST_CODE = 1
         const val ALARM_LOG = "ALARM_LOG"
     }
 }
